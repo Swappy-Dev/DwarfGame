d4935bcbd6ceb63a0d806f4e05f370b182a2ac85
Martin <staskunm@gmail.com>
2026-03-08 14:29:38 +0200

    Working on UI

--- Assets/TextMesh Pro/Examples & Extras/Scripts/SimpleScript.cs
+++ Assets/TextMesh Pro/Examples & Extras/Scripts/SimpleScript.cs
@@ -5,7 +5,7 @@ using System.Collections;
 namespace TMPro.Examples
 {
 
-    public class SimpleScript : MonoBehaviour
+    public class HealthHeart : MonoBehaviour
     {
 
         private TextMeshPro m_textMeshPro;
